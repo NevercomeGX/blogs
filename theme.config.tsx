c6cd46e305b6a161f385cb9466dbbfbce7f22261
NevercomeG <luisjavier2012@gmail.com>
2022-12-08 23:25:42 -0400

    1.0.1

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Nevercomex</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/NevercomeGX/",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/NevercomeGX/blogs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nevercomex Proyects © 2023",
   },
-}
+};
 
-export default config
+export default config;
