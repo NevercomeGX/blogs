3014df97e32115c0dc96cc0826f06e8b0d961da7
NevercomeG <luisjavier2012@gmail.com>
2022-12-11 23:55:21 -0400

    1.12.1

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Nevercomex</span>,
+  logo: <span>Nevercomex Docs</span>,
   project: {
     link: "https://github.com/NevercomeGX/",
   },
